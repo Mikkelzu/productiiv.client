2957359f182ce2665779700354f877cf2b1b3ba3
Michael <michael@mbp-van-michael.home>
2020-03-21 16:26:34 +0100

    added logos to the public folder

--- src/page-components/navbar.component.js
+++ src/page-components/navbar.component.js
@@ -9,7 +9,7 @@ export default class NavbarComponent extends React.Component {
             <div className="top-wrapper">
                 <div className="logo-wrapper d-inline-block">
                     <Link to="/">
-                        <img src="https://d33wubrfki0l68.cloudfront.net/e937e774cbbe23635999615ad5d7732decad182a/26072/logo-small.ede75a6b.svg" alt="NestJS - A progressive Node.js framework" />
+                        <img src='logo_small_icon_only_inverted.png' alt=""/>
                     </Link>
                 </div>
 
